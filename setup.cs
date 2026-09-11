cf62cc2da768319fc6608e664e6ddffb3811ef57
EEcoder7368 <eriks.dancitis@gmail.com>
2026-09-11 11:07:22 +0100

    Code uninstaller

--- setup.cs
+++ setup.cs
@@ -1,12 +1,25 @@
-/*using System;
+using System;
+using Arbot__V_Console___V_FileData_;
 
 namespace Setup
 {
-  public class program
+  public class Updater
   {
-    public static void Main(string[] args)
+    public static void update()
     {
       Console.WriteLine("Hi");
     }
   }
-}*/
+
+  public class Uninstaller
+  {
+    public static void uninstall()
+    {
+      File.Delete(Info.password_path);
+      File.Delete(Info.name_path);
+      File.Delete(Info.positives_path);
+      File.Delete(Info.negatives_path);
+      File.Delete(Info.password_reset_path);
+    }
+  }
+}
